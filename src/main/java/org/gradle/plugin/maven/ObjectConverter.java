c4615a09ffa5896bec1847ed8eefb59f215da800
Matthew McCullough <matthewm@ambientideas.com>
2011-06-30 16:16:33 -0600

    Revert "Awesome refactorings and improvements for EAR support from @dgileadi"
    This reverts commit 123e5aadd1b0b4f0ca5b5f83c719fdeb5a4bcd67.

--- src/main/java/org/gradle/plugin/maven/ObjectConverter.java
+++ src/main/java/org/gradle/plugin/maven/ObjectConverter.java
@@ -1,26 +1,22 @@
 package org.gradle.plugin.maven;
 
-import static com.google.common.collect.ImmutableMap.of;
-
 import java.util.Map;
+import static com.google.common.collect.ImmutableMap.of;
 
 class ObjectConverter {
 
     private static final String JAVA_PLUGIN_NAME = "java";
     private static final String WAR_PLUGIN_NAME = "war";
-    private static final String EAR_PLUGIN_NAME = "ear";
 
     private static final Map<String, String> packagingToPlugin = of(
             "jar", JAVA_PLUGIN_NAME,
-            "war", WAR_PLUGIN_NAME,
-            "ear", EAR_PLUGIN_NAME,
-            "ejb", JAVA_PLUGIN_NAME);   // TODO: use an actual EJB plugin
+            "war", WAR_PLUGIN_NAME);
 
     private static final Map<String, ? extends Map<String, String>> scopeToConfigurationAccordingToPackaging = of(
-            "compile", of("jar", "compile", "war", "compile", "ear", "deploy", "ejb", "compile"),
-            "test", of("jar", "testCompile", "war", "testCompile", "ear", "testCompile", "ejb", "testCompile"),
-            "runtime", of("jar", "runtime", "war", "runtime", "ear", "earlib", "ejb", "runtime"),
-            "provided", of("jar", "provided", "war", "providedCompile", "ear", "provided", "ejb", "provided"));
+            "compile", of("jar", "compile", "war", "compile"),
+            "test", of("jar", "testCompile", "war", "testCompile"),
+            "runtime", of("jar", "runtime", "war", "runtime"),
+            "provided", of("jar", "compile", "war", "providedCompile"));
 
     public static String packaging2Plugin(String packaging) {
         return packagingToPlugin.get(packaging);
